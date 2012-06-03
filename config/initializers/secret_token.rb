335faeeefaddfd3aa0915fd80d46e855a1ee738c
Ryan Lovelett <ryan@wahvee.com>
2012-06-03 12:44:01 -0500

    Base install.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExtjsRailsDemo::Application.config.secret_token = '9ad352c56cce30c092ae28ef820eff9d2f12bc790f6cda757a5c49044e1ed97e379900d4654c128dec136bf2601f458c45b65560c779e77db5200cfea7355970'
